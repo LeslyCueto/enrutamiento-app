f4fca1a2ec2e8fd546a3cf963a54e68f7d8a943c
Lesly Cueto <lesly.sharom@gmail.com>
2024-09-26 00:55:31 -0500

    busqueda terminada

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,14 +1,43 @@
-import React from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLoaderData, useSearchParams } from "react-router-dom";
 
 const Blog = () => {
   const { personajes } = useLoaderData();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    console.log(searchParams.get("categoria"));
+  }, [searchParams]);
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSearchParams({ personaje: e.target.value });
+  };
 
   return (
     <>
       <div className="container mt-2 p-3">
         <h2 className="text-primary">Blog</h2>
-        {personajes.map((item) => (
+        <input
+          className="form-control mb-2"
+          type="text"
+          onChange={handleChange}
+          value={
+            searchParams.get("personaje") ? searchParams.get("personaje") : ""
+          }
+        />
+        {personajes.filter((item) => {
+          let filter = searchParams.get('personaje');
+          let name = item.name.toLowerCase();
+
+          if (!filter) {
+            return true;
+          } else {
+            return name.startsWith(filter.toLowerCase());
+            
+          }
+        })
+        .map((item) => (
           <li key={item.id}>
             <Link to={`/blog/${item.id}`}>{item.name}</Link>
           </li>
--- src/pages/CardPersonaje.jsx
+++ src/pages/CardPersonaje.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData} from "react-router-dom";
 
 const CardPersonaje = () => {
   const { personajeInd } = useLoaderData();
-  console.log(personajeInd)
+  
 
-  return <div className="container mt-4">
-    <div className="row justify-content-center">
-        <div className="card col-4" style={{width:'18rem !important'}}>
-            <img src={personajeInd.image} alt={personajeInd.name} className="card-img-top" />
-            <div className="card-body">
-                <h5 className="card-title">Personaje: {personajeInd.name}</h5>
-            </div>
+  return (
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="card col-4" style={{ width: "18rem !important" }}>
+          <img
+            src={personajeInd.image}
+            alt={personajeInd.name}
+            className="card-img-top"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Personaje: {personajeInd.name}</h5>
+          </div>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default CardPersonaje;
